f833d9f85b506ac6356e78fd0dcb8f6f249b1425
litecodeai <176102639+litecodeai@users.noreply.github.com>
2024-07-23 16:22:17 +1000

    add demo video for ctrl + l

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+function tellJoke() {
+    console.log("Why don't scientists trust atoms?");
+    setTimeout(() => {
+        console.log("Because they make up everything!");
+    }, 2000);
+}
+
+tellJoke();
